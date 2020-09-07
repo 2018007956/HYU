b5d8755aea24d8e9904bd911ef7caff65ccdbe16
2018007956 <bbmt1906@hanyang.ac.kr>
2020-09-07 23:01:45 +0900

    To find root

--- HW1_bisection.py
+++ HW1_bisection.py
@@ -1,14 +1,15 @@
+# Bisection Method
 def f(x):
     return 5*pow(x,4)-22.4*pow(x,3)+15.85272*pow(x,2)+24.161472*x-23.4824832
 
 xl = int(input('lower bound : '))
 xu = int(input('upper bound : '))
 
-def bisection(xl, xu, tol):
+def bisection(xl, xu):
     if f(xl)*f(xu) > 0:
         print('No root found')
     else:
-        while (xu-xl)/2 > tol: #tolerance: 허용오차
+        while (xu-xl)/2 > 0.0000001: #tolerance: 허용오차
             m = (xu+xl)/2
             if f(m) == 0:
                 return m
@@ -18,5 +19,6 @@ def bisection(xl, xu, tol):
                 xu = m
         return m
 
-root = bisection(xl, xu, 0.0000000001)
+root = bisection(xl, xu)
 print('root: ', root)
+
